0bf22636f214bd3c535c69be973f59c3e5b2e9bb
lelderbe <lelderbe@student.21-school.ru>
2021-01-14 13:24:17 +0300

    fixed motion_event falling

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lelderbe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 12:12:35 by lelderbe          #+#    #+#             */
-/*   Updated: 2021/01/12 16:46:09 by lelderbe         ###   ########.fr       */
+/*   Updated: 2021/01/14 12:53:58 by lelderbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int		m_event(int button, int x, int y, t_params *e)
 {
 	int color;
 
+
+	//printf("e addr EVENT: %p\n", e);
 	(void)button;
 	color = 0xFFFFFF;
 	mlx_pixel_put(e->mlx, e->window, x, y, color);
@@ -24,17 +26,21 @@ int		m_event(int button, int x, int y, t_params *e)
 	return (0);
 }
 
-int		motion_event(t_params *e)
+int		motion_event(int x, int y, t_params *e)
 {
-	int x;
-	int y;
+	//int x;
+	//int y;
 
-	printf("e addr: %p\n", e);
-	printf("e->mlx addr: %p\n", e->mlx);
-	printf("before\n");
-	mlx_mouse_get_pos(e->window, &x, &y);
-	printf("after\n");
+	//(void)x;
+	//(void)y;
+	//printf("e addr: %p\n", e);
+	
+	//printf("e->mlx addr: %p\n", e->mlx);
+	//printf("before\n");
+	//mlx_mouse_get_pos(e->window, &x, &y);
+	//printf("after\n");
 	m_event(0, x, y, e);
+	
 	return (0);
 }
 
@@ -61,8 +67,10 @@ int		main(int argc, char **argv)
 	}
 	printf("e->mlx addr: %p\n", e.mlx);
 	printf("e->window addr: %p\n", e.window);
+	printf("e addr: %p\n", &e);
 	mlx_clear_window(e.mlx, e.window);
 	mlx_mouse_hook(e.window, &m_event, &e); 
+	printf("e addr 2: %p\n", &e);
 	mlx_hook(e.window, 6, 1L<<6, &motion_event, &e);
 	mlx_loop(e.mlx);
 	return (0);
